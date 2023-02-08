4bbe8b477286dee34d6a26950a7178f95e484653
EDDYBELDUMA150 <eddy.belduma.est@tecazuay.edu.ec>
2023-02-07 19:27:11 -0500

    Primer commit

---
+++ GestionPedidosM4B/src/main/java/com/ista/spring/GestionPedidosM4BApplication.java
@@ -0,0 +1,13 @@
+package com.ista.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPedidosM4BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPedidosM4BApplication.class, args);
+	}
+
+}
