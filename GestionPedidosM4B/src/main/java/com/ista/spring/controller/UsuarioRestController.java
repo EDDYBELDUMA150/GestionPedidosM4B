4ce166bacdc525ddba98be902dc0f9003dd4b17b
Miriam Medina <108770035+michilatte@users.noreply.github.com>
2023-02-27 22:40:47 -0500

    insertar el crossorigin e ingreso de datos en producto para pruebas

--- GestionPedidosM4B/src/main/java/com/ista/spring/controller/UsuarioRestController.java
+++ GestionPedidosM4B/src/main/java/com/ista/spring/controller/UsuarioRestController.java
@@ -9,6 +9,7 @@ import com.ista.spring.models.services.IUsuarioService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins=("http://localhost:4200"))
 public class UsuarioRestController {
 
 	@Autowired
---
+++ GestionPedidosM4B/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_preciounitario`) VALUES ('Aceite','nose','tampoco se',14);
+INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_preciounitario`) VALUES ('Aceite1','nose','tampoco se',14);
+INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_preciounitario`) VALUES ('Aceite2','nose','tampoco se',14);
+INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_preciounitario`) VALUES ('Aceite4','nose','tampoco se',14);
+INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_preciounitario`) VALUES ('Aceite5','nose','tampoco se',14);
+
