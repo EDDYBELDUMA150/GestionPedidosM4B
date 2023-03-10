e35d1da076f2654d9cf1f41379174b597c8879e4
USUARIO <USUARIO@CAB3ZON>
2023-03-10 08:21:45 -0500

    AAAAAA

--- GestionPedidosM4B/src/main/resources/import.sql
+++ GestionPedidosM4B/src/main/resources/import.sql
@@ -4,3 +4,6 @@ INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_pr
 INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_preciounitario`) VALUES ('Aceite4','nose','tampoco se',14);
 INSERT INTO `productos`(`prod_nombre`, `prod_tipo`, `prod_descripcion`, `prod_preciounitario`) VALUES ('Aceite5','nose','tampoco se',14);
 
+
+INSERT INTO `clientes`(`cli_identificacion`,`cli_nombres`,`cli_apellidos`,`cli_correo`,`cli_clave`,`cli_telefono`)VALUES('0150066686','Marcos Horacio','Pillco Contreras','marcospillcocontreras@gmail.com','marcos2001=','0958814375');
+
